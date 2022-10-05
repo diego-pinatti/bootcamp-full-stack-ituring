dab49b36bcd9207c2535b9ac7d9b16055dd56a66
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-05 19:19:45 -0300

    exibir valores na pratica 1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,3 +3,9 @@ let email = "diego.pinatti87@yahoo.com"
 let profissao = "analista de configuração"
 let idade = 34
 let lugar = "Japão"
+
+console.log(name)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
