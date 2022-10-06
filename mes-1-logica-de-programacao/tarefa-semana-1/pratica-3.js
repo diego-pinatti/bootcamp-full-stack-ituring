0759567c5b4068d4f429666cf02247f6a834b3cb
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-06 18:43:51 -0300

    finalizar pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+let name = "Diego"
+let email = "diego.pinatti87@yahoo.com"
+let profissao = "analista de configuração"
+let idade = 34
+let lugar = "Japão"
+
+console.log(typeof name == "string")
+console.log(typeof email == "string")
+console.log(typeof profissao == "string")
+console.log(typeof idade == "string")
+console.log(typeof lugar == "string")
