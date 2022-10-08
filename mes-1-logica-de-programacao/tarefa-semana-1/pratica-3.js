2fe214ec28bf9f9eb5a2e2b06a74c94599e49ad9
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-08 08:40:23 -0300

    colocado parenteses no typeof da pratica 3

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -4,8 +4,8 @@ let profissao = "analista de configuração"
 let idade = 34
 let lugar = "Japão"
 
-console.log(typeof name == "string")
-console.log(typeof email == "string")
-console.log(typeof profissao == "string")
-console.log(typeof idade == "string")
-console.log(typeof lugar == "string")
+console.log(typeof (name) == "string")
+console.log(typeof (email) == "string")
+console.log(typeof (profissao) == "string")
+console.log(typeof (idade) == "string")
+console.log(typeof (lugar) == "string")
