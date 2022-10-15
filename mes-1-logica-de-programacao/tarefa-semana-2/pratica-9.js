521725d0a94b04888e54841ab20a3a1bd9d4c02e
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-15 14:47:00 -0300

    concluido pratica 9 - tarefa semana 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,11 @@
+let qualEhMaior = (x, y) => {
+    if (x > y) {
+        return x   
+    }if (y > x) {
+        return y    
+    }if (x = y) {
+        return x
+    }
+}
+
+console.log (qualEhMaior (0.1, 0.01))
