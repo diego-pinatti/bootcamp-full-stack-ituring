f38015990b4a9b5e092337a26ffbd9759e75ab85
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-19 17:36:29 -0300

    concluido pratica 01 - tarefa semana 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,12 @@
+function criarTabuada (n) {
+    let i = 1
+
+    console.log(`Tabuada de ${n}`)
+
+    while (i <= 10) {
+        console.log(`${n} x ${i} = ${n * i}`)
+        i++
+    }
+}
+
+criarTabuada(12)
