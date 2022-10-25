8cab5af0ded9d3377af0f27f1b12ef44fc8fb65e
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-25 18:34:10 -0300

    alterado pratica 10 - tarefa semana 3

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
@@ -1,31 +1,50 @@
-function totalVotos (lista) {
+function verificaPreferidoDaCopa (votosBrasil, votosArgentina, votosEspanha, votosPortugal) {
+    
+    if(votosBrasil >= votosArgentina && votosBrasil >= votosEspanha && votosBrasil >= votosPortugal) {
+        return "Brasil"
+    } else if(votosArgentina >= votosEspanha && votosArgentina >= votosPortugal) {
+        return "Argentina"
+    } else if(votosEspanha >= votosPortugal) {
+        return "Espanha"
+    } else {
+        return "Portugal"
+    }
+}
+
+function exibepreferidodaCopa (listaVotos) {
     let votosBrasil = 0
     let votosArgentina = 0
     let votosEspanha = 0
     let votosPortugal = 0
 
-    for(let i = 0; i < lista.length; i++) {
+    for(let i = 0; i < listaVotos.length; i++) {
         
-        if(lista[i] == 1) {
+        if(listaVotos[i] == 1) {
             votosBrasil++
-        } if(lista[i] == 2) {
+        } if(listaVotos[i] == 2) {
             votosArgentina++
-        } if(lista[i] == 3) {
+        } if(listaVotos[i] == 3) {
             votosEspanha++
-        } if(lista[i] == 4) {
+        } if(listaVotos[i] == 4) {
             votosPortugal++
         }
     }
+
+    let preferido = verificaPreferidoDaCopa(votosBrasil, votosArgentina, votosEspanha, votosPortugal)
 
     console.log(`Brasil: ${votosBrasil} voto(s)`)
     console.log(`Argentina: ${votosArgentina} voto(s)`)
     console.log(`Espanha: ${votosEspanha} voto(s)`)
     console.log(`Portugal: ${votosPortugal} voto(s)`)
-    
+    console.log(`\n`)
+    console.log("Resultado:")
+    console.log(`${preferido} é o preferido para ganhar a Copa do Mundo!`)
 }
 
+const listaVotos = [1, 1, 1, 4, 4, 2, 3, 2]
+exibepreferidodaCopa(listaVotos)
 
-const lista = [1, 1, 1, 4, 4, 2, 3, 2]
 
-timePreferido(lista)
-totalVotos(lista)
+
+
+
