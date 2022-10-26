dedcfeedeb82ee3010327fe3e00c0383b06af800
Diego Batista <diego.pinatti87@yahoo.com>
2022-10-26 18:31:58 -0300

    concluido pratica 01 - tarefa semana 4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,5 @@
+let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+lista.forEach(function(item, i) {
+    console.log(`[${[i]}] ${lista[i]}`)
+})
