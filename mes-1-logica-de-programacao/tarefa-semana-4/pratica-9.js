755e784d5fa13aadb0a2861dace46730f4923575
Diego Batista <diego.pinatti87@yahoo.com>
2022-11-01 18:54:28 -0300

    concluido pratica 09 - tarefa semana 4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-9.js
@@ -0,0 +1,31 @@
+const listaCrescente = (lista) => {
+
+    let listaOrdenada = lista.sort(function(a, b) {
+        return b.notaAvaliacao - a.notaAvaliacao
+    })
+    console.log(`O filme de melhor avaliação com nota ${lista[0].notaAvaliacao} é: ${lista[0].titulo}.`)    
+}
+
+let listaDeFilmes = [{
+    titulo: "Um Sonho de Liberdade",
+    notaAvaliacao: 9.2,
+    duracao: 142,
+    anoPublicacao: 1994,
+    categoria: "Drama"
+  },
+  {
+    titulo: "Clube da Luta",
+    notaAvaliacao: 8.7,
+    duracao: 139,
+    anoPublicacao: 1999,
+    categoria: "Drama"
+  },
+  {
+    titulo: "Toy Story 3",
+    notaAvaliacao: 8.3,
+    duracao: 103,
+    anoPublicacao: 2010,
+    categoria: "Animação"
+  }]
+
+listaCrescente(listaDeFilmes)
