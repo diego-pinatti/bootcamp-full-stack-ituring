8064fb03ce1fb8478ed09e6b7eaafc069afcd703
Diego Batista <diego.pinatti87@yahoo.com>
2022-11-02 14:11:57 -0300

    concluido pratica 11 - tarefa semana 4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-11.js
@@ -0,0 +1,58 @@
+function calculoFinal(lista) {    
+    let saldoPositivo = 0
+    let saldoNegativo = 0
+
+    lista.forEach(function(item) {
+        if (item.movimentacao === "deposito") {
+            return saldoPositivo += item.valor
+        } else if (item.movimentacao === "retirada") {
+            return saldoNegativo += item.valor
+        }
+    })
+
+    let saldoFinal = saldoPositivo - saldoNegativo
+    let positivoOuNegativo = saldoFinal >= 0 ? "positivo" : "negativo"
+
+    console.log(`O valor total depositado foi de: ${saldoPositivo}`)
+    console.log(`O valor total retirado foi de: ${saldoNegativo}`)
+    console.log(`O saldo final de sua conta foi ${positivoOuNegativo} no valor de R$ ${saldoFinal}.`)
+}    
+
+let dadosBancarios = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]
+
+
+  calculoFinal(dadosBancarios)
