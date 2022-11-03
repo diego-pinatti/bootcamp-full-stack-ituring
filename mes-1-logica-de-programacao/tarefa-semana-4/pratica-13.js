223abb6e7eb20c4e995de3c77883690c8d89d23e
Diego Batista <diego.pinatti87@yahoo.com>
2022-11-03 19:34:27 -0300

    atualizado pratica 13 - tarefa semana 4

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-13.js
@@ -0,0 +1,52 @@
+const ordenarCrescente = (lista) => {
+
+    lista.forEach(function(item) {
+        item.dataMovimentacao == String
+          return item.dataMovimentacao = new Date(item.dataMovimentacao)        
+    })
+
+    lista.sort(function(a, b) {
+        return b.dataMovimentacao - a.dataMovimentacao
+    })
+
+    console.log(lista)
+}
+
+
+let dadosBancarios = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]
+
+  ordenarCrescente(dadosBancarios)
